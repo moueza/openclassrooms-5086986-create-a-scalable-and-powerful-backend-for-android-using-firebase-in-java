99241d3cdb2c1baa63a0994ad603d8458cefee12
moueza <>
2021-02-08 00:30:18 +0100

    see #33 p1c3part3 Manage the authentication feedback ; see #34 Bug R.string ; KO

--- FirebaseOC/app/src/main/java/com/openclassrooms/firebaseoc/MainActivity.java
+++ FirebaseOC/app/src/main/java/com/openclassrooms/firebaseoc/MainActivity.java
@@ -1,13 +1,19 @@
 package com.openclassrooms.firebaseoc;
 
+import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.openclassrooms.firebaseoc.base.BaseActivity;
 
 import java.util.Arrays;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
@@ -49,5 +55,53 @@ public class MainActivity extends BaseActivity {
                         .setLogo(R.drawable.ic_logo_auth)
                         .build(),
                 RC_SIGN_IN);
+    }
+
+
+    //FOR DESIGN
+    // 1 - Get Coordinator Layout
+    @BindView(R.id.main_activity_coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 4 - Handle SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+
+    // --------------------
+    // UI
+    // --------------------
+
+    // 2 - Show Snack Bar with a message
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message) {
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    // 3 - Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+                showSnackBar(this.coordinatorLayout, getString(R.string.connection_succeed));
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.coordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
     }
 }
