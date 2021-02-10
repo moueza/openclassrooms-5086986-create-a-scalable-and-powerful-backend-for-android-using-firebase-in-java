80072e588c6485757546c22f35d06401a9fc1acd
moueza <>
2021-02-10 09:45:08 +0100

    resolves #42 p1c5part2 Retrieve the user's information ; see #1 OK emu 26

--- FirebaseOC/app/src/main/java/com/openclassrooms/firebaseoc/auth/ProfileActivity.java
+++ FirebaseOC/app/src/main/java/com/openclassrooms/firebaseoc/auth/ProfileActivity.java
@@ -2,10 +2,13 @@ package com.openclassrooms.firebaseoc.auth;
 
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
+import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.openclassrooms.firebaseoc.R;//moi
 import com.openclassrooms.firebaseoc.base.BaseActivity;
 
@@ -28,6 +31,8 @@ public class ProfileActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.configureToolbar();
+        this.updateUIWhenCreating(); // 2 - Update UI
+
     }
 
     @Override
@@ -49,5 +54,33 @@ public class ProfileActivity extends BaseActivity {
 
     @OnClick(R.id.profile_activity_button_delete)
     public void onClickDeleteButton() {
+    }
+
+
+    // --------------------
+    // UI
+    // --------------------
+
+    // 1 - Update UI when activity is creating
+    private void updateUIWhenCreating(){
+
+        if (this.getCurrentUser() != null){
+
+            //Get picture URL from Firebase
+            if (this.getCurrentUser().getPhotoUrl() != null) {
+                Glide.with(this)
+                        .load(this.getCurrentUser().getPhotoUrl())
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(imageViewProfile);
+            }
+
+            //Get email & username from Firebase
+            String email = TextUtils.isEmpty(this.getCurrentUser().getEmail()) ? getString(R.string.info_no_email_found) : this.getCurrentUser().getEmail();
+            String username = TextUtils.isEmpty(this.getCurrentUser().getDisplayName()) ? getString(R.string.info_no_username_found) : this.getCurrentUser().getDisplayName();
+
+            //Update views with data
+            this.textInputEditTextUsername.setText(username);
+            this.textViewEmail.setText(email);
+        }
     }
 }
